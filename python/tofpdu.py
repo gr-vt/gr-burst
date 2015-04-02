2a6f865d35ae60c34527ca2032163264ef817b05
Tim O'Shea <tim.oshea753@gmail.com>
2015-04-01 17:22:53 -0700

    fix tofpdu block

--- python/tofpdu.py
+++ python/tofpdu.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 from gnuradio import gr;
-import pmt, sys, pprint, bitarray, array, struct, binascii
+import numpy, pmt, sys, pprint, bitarray, array, struct, binascii
 
 class tofpdu(gr.sync_block):
     def __init__(self, blocksize=1024):
-        gr.sync_block.__init__(self,"binary_slicer_async",[],[])
+        gr.sync_block.__init__(self,"binary_tofpdu",[],[])
         self.message_port_register_in(pmt.intern("pdus"));
         self.message_port_register_out(pmt.intern("fpdus"));
         self.set_msg_handler(pmt.intern("pdus"), self.handler);
@@ -15,13 +15,8 @@ class tofpdu(gr.sync_block):
         meta = pmt.car(msg);
         data = pmt.cdr(msg);
         
-        # convert pmt -> int list (of bits)
-        data = array.array('B', pmt.u8vector_elements(data))
-        bindata = map(lambda x: float(x)*2.0-1.0, data);
-        bindatap = pmt.init_f32vector(len(bindata), bindata);
-
-        # make the new pdu
-        pdu = pmt.cons(meta, bindatap);
+        f_vec = pmt.to_pmt(  numpy.array(pmt.to_python(data), dtype="float32") * 2 - 1 )
+        pdu = pmt.cons(meta, f_vec);
 
         # send it on its way
         self.message_port_pub(pmt.intern("fpdus"), pdu);
